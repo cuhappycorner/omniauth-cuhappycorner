9349ceba7b9cf5e9663881b7ca4393864155dde9
Dicky Ho <me@ckho.info>
2017-03-20 22:31:36 +0800

    Bug Fix

--- lib/omniauth/cuhappycorner.rb
+++ lib/omniauth/cuhappycorner.rb
@@ -20,20 +20,21 @@ module OmniAuth
         raw_info["_id"]["$oid"]
       }
 
-      info {
-        :email => raw_info["email"],
-        :birthday => raw_info["birthday"],
-        :mobile => raw_info["mobile"],
-        :display_name => raw_info["display_name"],
-        :name => raw_info["name"],
-        :gender => raw_info["gender"],
-        :cu_resident => raw_info["cu_resident"],
-        :cu_link_id => raw_info["cu_link_id"],
-        :type => raw_info[""],
-        :token => access_token.token
-        # raw_info.merge("token" => access_token.token)
-        # and anything else you want to return to your API consumers
-      }
+      info do {
+          :email => raw_info["email"],
+          :birthday => raw_info["birthday"],
+          :mobile => raw_info["mobile"],
+          :display_name => raw_info["display_name"],
+          :name => raw_info["name"],
+          :gender => raw_info["gender"],
+          :cu_resident => raw_info["cu_resident"],
+          :cu_link_id => raw_info["cu_link_id"],
+          :type => raw_info[""],
+          :token => access_token.token
+          # raw_info.merge("token" => access_token.token)
+          # and anything else you want to return to your API consumers
+        }
+      end
 
       def raw_info
         @raw_info ||= access_token.get('/api/v1/me.json').parsed
